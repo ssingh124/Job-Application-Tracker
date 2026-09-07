3c8013c4a85ed1ed7fdc544b85a16987fab0ff3b
Satnam Singh <sonuthandi85@gmail.com>
2026-09-07 13:00:02 -0700

    adding new feature of move job board to Interview,Accepted,Rejected

--- script.js
+++ script.js
@@ -10,10 +10,7 @@ const acceptedContainer = document.getElementById("jobAccepted");
 
 //Listening to Submit button Press
 form.addEventListener("submit", function(event) {
-let appliedHTML = "";
-let interviewHTML = "";
-let rejectedHTML = "";
-let acceptedHTML = "";
+
 
     event.preventDefault();  //By default, when a form is submitted, the browser tries to submit/reload the page. Since we're handling the form with JavaScript, we usually want to stop that.
 
@@ -28,8 +25,9 @@ let acceptedHTML = "";
     const position= positionInput.value;  
     const status= statusInput.value;  
     
-    // Create a brand new object with those values
+    // Creating a brand new object with those values
   const job = {
+    id: crypto.randomUUID(),
     date: date,
     companyName: companyName,
     position: position,
@@ -40,10 +38,20 @@ let acceptedHTML = "";
 
     jobs.push(job);
 
+   renderJobs();
 
 
 
-//Section - 2------------------------------------------------------------
+});
+
+
+
+//Functions---------------
+
+function renderJobs(){
+
+
+  //Section - 2------------------------------------------------------------
 
 //Filtering object and checking - Status:Applied total
 const appliedJobs = jobs.filter(function(job) {
@@ -73,37 +81,50 @@ document.getElementById("totalAccepted").textContent = acceptedJobs.length;
 
 // Section 3 - Displaying values of array of objects to html-----------------------------------------
 
+let appliedHTML = "";
+let interviewHTML = "";
+let rejectedHTML = "";
+let acceptedHTML = "";
 
-//Run
+
 // Running loop for jobs array of Objects
 jobs.forEach(job => {
     
     //Checking jobs array and then adding values to HTML tags
     if (job.status === "applied") {
         appliedHTML += `
-    <div class="job-card" style="border: 1px solid #ccc; padding: 10px; margin: 10px 0;">
+     <div class="job-card" data-job-id="${job.id}">
       <p><strong>Date:</strong> ${job.date}</p>
       <p><strong>Company:</strong> ${job.companyName}</p>
       <p><strong>Position:</strong> ${job.position}</p>
       <p><strong>Status:</strong> ${job.status}</p>
+        <button type="button" class="move-button move-to-interview" data-job-id="${job.id}">
+            Move to Interview
+        </button>
     </div>
   `;
     }
 
     else if (job.status === "interview") {
         interviewHTML += `
-    <div class="job-card" style="border: 1px solid #ccc; padding: 10px; margin: 10px 0;">
+    <div class="job-card" data-job-id="${job.id}">
       <p><strong>Date:</strong> ${job.date}</p>
       <p><strong>Company:</strong> ${job.companyName}</p>
       <p><strong>Position:</strong> ${job.position}</p>
       <p><strong>Status:</strong> ${job.status}</p>
+      <button type="button" class="move-button move-to-rejected" data-job-id="${job.id}">
+            Rejected
+        </button>
+        <button type="button" class="move-button move-to-accepted" data-job-id="${job.id}">
+            Accepted
+        </button>
     </div>
   `;
     }
 
     else if (job.status === "rejected") {
         rejectedHTML += `
-    <div class="job-card" style="border: 1px solid #ccc; padding: 10px; margin: 10px 0;">
+     <div class="job-card" data-job-id="${job.id}">
       <p><strong>Date:</strong> ${job.date}</p>
       <p><strong>Company:</strong> ${job.companyName}</p>
       <p><strong>Position:</strong> ${job.position}</p>
@@ -114,7 +135,7 @@ jobs.forEach(job => {
 
     else if (job.status === "accepted") {
         acceptedHTML += `
-    <div class="job-card" style="border: 1px solid #ccc; padding: 10px; margin: 10px 0;">
+    <div class="job-card" data-job-id="${job.id}">
       <p><strong>Date:</strong> ${job.date}</p>
       <p><strong>Company:</strong> ${job.companyName}</p>
       <p><strong>Position:</strong> ${job.position}</p>
@@ -132,5 +153,63 @@ interviewContainer.innerHTML = interviewHTML;
 rejectedContainer.innerHTML = rejectedHTML;
 acceptedContainer.innerHTML = acceptedHTML;
 
+}
+
+//LISTENING TO MOVE JOB BUTTON
+
+document.addEventListener("click", function(event) {
+
+    if (event.target.classList.contains("move-to-interview")) {
+      //Assigning id of the object for ehich move button was click/ what job objects status has to be changed
+        const jobId = event.target.dataset.jobId;
+      //matching the Job id received to the array of objects and getting object
+        const job = jobs.find(function(job) {
+         return job.id === jobId;
+      });
+      if (job) {
+        //Changing the status of the job to Interview to move it to interview column
+            job.status = "interview";
+        }
+
+      // Running render page function that will referesh the Jabs board and move all the jobs to there designated column based on status
+      renderJobs();
+
+    }
+       
+    
+    if (event.target.classList.contains("move-to-rejected")) {
+      //Assigning id of the object for which move button was click/ what job objects status has to be changed
+        const jobId = event.target.dataset.jobId;
+      //matching the Job id received to the array of objects and getting object
+        const job = jobs.find(function(job) {
+         return job.id === jobId;
+      });
+      if (job) {
+        //Changing the status of the job to Rejected to move it to Rejected column
+            job.status = "rejected";
+        }
+
+      // Running render page function that will referesh the Jabs board and move all the jobs to there designated column based on status
+      renderJobs();
+
+    }
+
+
+     if (event.target.classList.contains("move-to-accepted")) {
+      //Assigning id of the object for which move button was click/ what job objects status has to be changed
+        const jobId = event.target.dataset.jobId;
+      //matching the Job id received to the array of objects and getting object
+        const job = jobs.find(function(job) {
+         return job.id === jobId;
+      });
+      if (job) {
+        //Changing the status of the job to Accepted to move it to Accepted column
+            job.status = "accepted";
+        }
+
+      // Running render page function that will referesh the Jabs board and move all the jobs to there designated column based on status
+      renderJobs();
+
+    }
 
 });
